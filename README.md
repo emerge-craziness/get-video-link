015fbf769174e04ddeb1e0d212d6dccadfc574e6
emerge-craziness <ispikulin@yandex.ru>
2015-03-09 04:37:38 +0300

    night commit 1. Now it can get the maximum quality 2. All the error messages are redirected to the STDERR

--- README.md
+++ README.md
@@ -1,8 +1,7 @@
-# get-video-link
-This small script can quickly return the straight video link by its embed-code (for VK videos)
+# gvl.sh
+It can quickly return the straight video links for VK-uploaded videos.
 
 # Usage
 `gvl.sh [quality]`<br>
-...then just paste the `"rightclick -> Copy video code"` into stdin.<br>
-The script will print the straight placement of this video on the servers,
-so you will get the possibility of downloading it.
+...then just put the `"rightclick -> Copy video code"` into stdin.<br>
+The script will get you the straight link to the video.
--- gvl.sh
+++ gvl.sh
@@ -17,33 +17,48 @@ elif [[ $QUALITY = "480" ]] then
   FIELD_NUM=6
 elif [[ $QUALITY = "720" ]] then
   FIELD_NUM=8
+elif [[ $QUALITY = "max" ]] then
+  #nothing
 else 
-  echo "Invalid quality value"
+  echo "Invalid quality value." 1>&2 #stderr
+  echo "It must be in {240,360,480,720,max}" 1>&2 #stderr
   return 1
 fi
 
 # Getting the link refers to the video page:
 read CODE
-echo $CODE | cut -d "\"" -f 2 | read LINK
+LINK=$(echo $CODE | cut -d "\"" -f 2)
 
 # Test line
 #echo $LINK
 
 # Getting the straight links:
 GOT_CODE=$(exec curl -s "$LINK")
-USEFUL_CODE=$(echo $GOT_CODE | grep "url240" | head -n 1 | sed "s/.*url240/url240/")
+USEFUL_CODE=$(echo $GOT_CODE | grep "url240" | head -n 1 | sed "s/.*url240/url240/" | sed "s/cache.*//")
+if [[ $1 = "max" ]] then
+  NUM_OF_SEPARATORS=$(echo $USEFUL_CODE | grep -o "=" | wc -l)
+  FIELD_NUM=$NUM_OF_SEPARATORS
+fi
 RESULT=$(echo $USEFUL_CODE | cut -d "=" -f $FIELD_NUM | cut -d "?" -f 1 | sed "s/https:/http:/")
 
-# Test line
-#echo $RESULT
 
-if ! [[ x$RESULT = x ]] && [[ x`echo $RESULT | grep ".mp4" | grep "$QUALITY"` = x$RESULT ]] then
+
+# Test line(s)
+#echo "echo USEFUL_CODE | grep -o "=" | wc -l ==== `echo $USEFUL_CODE | grep -o "=" | wc -l`"
+#echo "Useful_code is $USEFUL_CODE"
+#echo "Result is $RESULT"
+#echo $NUM_OF_SEPARATORS
+
+
+
+if ! [[ x$RESULT = x ]] then
   echo $RESULT
   return 0
-elif ! [[ x$RESULT = x ]] then
-  echo "Nonexistent video quality $QUALITY" #stderr
+elif ! [[ x$USEFUL_CODE = x ]] then
+  echo "Nonexistent video quality $QUALITY." 1>&2 #stderr
+  echo "Use \`$0 max\` to get the highest possible quality" 1>&2 #stderr
   return 1
 else
-  echo "Connection error"
+  echo "Connection error" 1>&2 #stderr
   return 1
 fi
